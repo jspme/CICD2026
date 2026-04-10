ccd4dbd1dd1378df6f674206b011560ff6c4d6a3
jspme <2157216@cegepmontpetit.ca>
2026-04-10 15:48:45 -0400

    Changer le message pour dev

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Bonjour de test l'environnement de DEV",
+  dev: "Bonjour del'environnement de DEV",
   qa: "Hello de l'environnement de QA",
   prod: "Hello de l'environnement de PROD",
 };
